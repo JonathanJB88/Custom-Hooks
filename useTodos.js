bec3862128bda2fdc6e397c56a51d2129f6590b2
JonathanJB88 <Jonajes0288@gmail.com>
2022-10-26 17:05:02 +0200

    First commit

---
+++ useTodos.js
@@ -0,0 +1,41 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const init = () => {
+  return JSON.parse(localStorage.getItem("todos")) || [];
+};
+
+export const useTodos = () => {
+  const [todos, dispatch] = useReducer(todoReducer, [], init);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const onNewTodo = (newTodo) => {
+    const action = { type: "[TODO] Add Todo", payload: newTodo };
+    dispatch(action);
+  };
+
+  const onDeleteTodo = (id) => {
+    const action = { type: "[TODO] Remove Todo", payload: id };
+    dispatch(action);
+  };
+
+  const onToggleTodo = (id) => {
+    const action = { type: "[TODO] Toggle Todo", payload: id };
+    dispatch(action);
+  };
+
+  const todosCount = todos.length;
+  const pendingTodosCount = todos.filter((todo) => !todo.done).length;
+
+  return {
+    todos,
+    onNewTodo,
+    onDeleteTodo,
+    onToggleTodo,
+    todosCount,
+    pendingTodosCount,
+  };
+};
